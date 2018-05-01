e1f0b88c5fe4a87ab439d6265b8c9a7656854299
John Harrison <john.randolph.harrison@gmail.com>
2018-04-30 22:21:34 -0500

    interface for the repo

--- data/eggrepository.go
+++ data/eggrepository.go
@@ -1,44 +1,14 @@
 package data
 
-import "hatchery/models"
+import (
+	"hatchery/models"
+)
 
-var eggs []models.Egg
-var index = 0
-
-//TODO: add function to fetch into memory
-
-//StoreEgg will put an egg into either memory or db storage depending on how far out it should hatch
-func StoreEgg(egg models.Egg) {
-	egg.Id = index
-	index++
-
-	for i, item := range eggs {
-		if egg.HatchTime.Before(item.HatchTime) {
-			eggs = append(eggs[:i], append([]models.Egg{egg}, eggs[i:]...)...)
-			return
-		}
-	}
-	eggs = append(eggs, egg)
-}
-
-//RetrieveEgg gets an egg by Id
-func RetrieveEgg(id int) models.Egg {
-	//do nothing atm
-	var egg models.Egg
-	return egg
-}
-
-//RetrieveEggs gets all eggs
-func RetrieveEggs() []models.Egg {
-	return eggs
-}
-
-//RemoveEgg removes an egg by Id
-func RemoveEgg(id int) {
-	eggs = eggs[1:]
-}
-
-//RemoveEggs removes n eggs from memory
-func RemoveEggs(number int) {
-	eggs = eggs[number:]
+//EggRepository is the interface for access eggs from whatever data layer.
+type EggRepository interface {
+	StoreEgg(egg models.Egg)
+	RetrieveEgg(id int) models.Egg
+	RetrieveEggs() []models.Egg
+	RemoveEgg(id int)
+	RemoveEggs(number int)
 }
---
+++ data/postgreeggrepository.go
@@ -0,0 +1,44 @@
+package data
+
+import "hatchery/models"
+
+var eggs []models.Egg
+var index = 0
+
+//TODO: add function to fetch into memory
+
+//StoreEgg will put an egg into either memory or db storage depending on how far out it should hatch
+func StoreEgg(egg models.Egg) {
+	egg.Id = index
+	index++
+
+	for i, item := range eggs {
+		if egg.HatchTime.Before(item.HatchTime) {
+			eggs = append(eggs[:i], append([]models.Egg{egg}, eggs[i:]...)...)
+			return
+		}
+	}
+	eggs = append(eggs, egg)
+}
+
+//RetrieveEgg gets an egg by Id
+func RetrieveEgg(id int) models.Egg {
+	//do nothing atm
+	var egg models.Egg
+	return egg
+}
+
+//RetrieveEggs gets all eggs
+func RetrieveEggs() []models.Egg {
+	return eggs
+}
+
+//RemoveEgg removes an egg by Id
+func RemoveEgg(id int) {
+	eggs = eggs[1:]
+}
+
+//RemoveEggs removes n eggs from memory
+func RemoveEggs(number int) {
+	eggs = eggs[number:]
+}
